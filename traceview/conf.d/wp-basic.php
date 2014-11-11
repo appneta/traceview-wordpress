9b88f92125e7932e380a2952c4b7f8e20a1f6ee9
Greg Bromage <gbromage@appneta.com>
2014-11-10 17:16:17 -0800

    Comment out some less commonly used hooks
    Signed-off-by: Greg Bromage <gbromage@appneta.com>

--- traceview/conf.d/wp-basic.php
+++ traceview/conf.d/wp-basic.php
@@ -1,19 +1,20 @@
 <?php
 /* Wordpress basic hooks */
-add_action('wp_before_admin_bar_render','traceview_start_section',1);
-add_action('wp_after_admin_bar_render','traceview_end_section',254);
+
+# add_action('wp_before_admin_bar_render','traceview_start_section',1);
+# add_action('wp_after_admin_bar_render','traceview_end_section',254);
 
 add_action('comment_form_before','traceview_start_section',1);
 add_action('comment_form_after','traceview_end_section',254);
 
-add_action('comment_form_before_fields','traceview_start_section',1);
-add_action('comment_form_after_fields','traceview_end_section',254);
+# add_action('comment_form_before_fields','traceview_start_section',1);
+# add_action('comment_form_after_fields','traceview_end_section',254);
 
 add_action('dynamic_sidebar_before','traceview_start_section',1);
 add_action('dynamic_sidebar_after','traceview_end_section',254);
 
-add_action('before_signup_form','traceview_start_section',1);
-add_action('after_signup_form','traceview_end_section',254);
+# add_action('before_signup_form','traceview_start_section',1);
+# add_action('after_signup_form','traceview_end_section',254);
 
 add_action('before_delete_post','traceview_start_section',1);
 add_action('after_delete_post','traceview_end_section',254);
@@ -21,14 +22,11 @@ add_action('after_delete_post','traceview_end_section',254);
 add_action('loop_start','traceview_start_section',1);
 add_action('loop_end','traceview_end_section',254);
 
-add_action('comment_form_before','traceview_start_section',1);
-add_action('comment_form_after','traceview_end_section',254);
+# add_action('comment_form_logged_in','traceview_start_section',1);
+# add_action('comment_form_Logged_in_after','traceview_end_section',254);
 
-add_action('comment_form_logged_in','traceview_start_section',1);
-add_action('comment_form_Logged_in_after','traceview_end_section',254);
-
-add_action('before_wp_tiny_mce','traceview_start_section',1);
-add_action('after_wp_tiny_mce','traceview_end_section',254);
+# add_action('before_wp_tiny_mce','traceview_start_section',1);
+# add_action('after_wp_tiny_mce','traceview_end_section',254);
 
 add_action('setup_theme','traceview_start_section',1);
 add_action('after_setup_theme','traceview_end_section',254);
@@ -44,7 +42,7 @@ tv_action_watch('the_post');
 
 tv_filter_watch('the_posts');
 tv_filter_watch('the_content');
-tv_filter_watch('the_comments');
+# tv_filter_watch('the_comments');
 
 tv_action_watch('wp_head');
 tv_action_watch('wp_meta');
@@ -59,15 +57,18 @@ tv_action_watch('admin_head');
 tv_action_watch('admin_footer');
 tv_action_watch('admin_init');
 tv_action_watch('admin_body_class');
-tv_action_watch('pre_get_posts');
-tv_action_watch('pre_get_comments');
+# tv_action_watch('pre_get_posts');
+# tv_action_watch('pre_get_comments');
+
+# --------------------------------
 # Do not trace this hook - doing so will break the Plugin
 # search screen
 # tv_action_watch('pre_http_request');
+# --------------------------------
+
 tv_action_watch('generate_rewrite_rules');
 tv_action_watch('comment_loop_start');
 
-
-add_action('get_header','traceview_log_hook');
-add_action('get_footer','traceview_log_hook');
+# add_action('get_header','traceview_log_hook');
+# add_action('get_footer','traceview_log_hook');
 ?>
