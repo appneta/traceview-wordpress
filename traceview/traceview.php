fb579e32dc50f677dde427d894be71907963ca95
Greg Bromage <gbromage@appneta.com>
2015-05-07 09:34:03 -0700

    - Fix code indenting/formatting (no functional change)
    Signed-off-by: Greg Bromage <gbromage@appneta.com>

--- traceview/traceview.php
+++ traceview/traceview.php
@@ -333,23 +333,23 @@ function traceview_annotate($message) {
 		    'username' => urlencode($current_user->user_login)
 		);
 
-	foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-	rtrim($fields_string, '&');
+		foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+		rtrim($fields_string, '&');
 
 	//open connection
-	$ch = curl_init();
+		$ch = curl_init();
 
 	//set the url, number of POST vars, POST data
-	curl_setopt($ch,CURLOPT_URL, $url);
-	curl_setopt($ch,CURLOPT_POST, count($fields));
-	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-	curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch,CURLOPT_URL, $url);
+		curl_setopt($ch,CURLOPT_POST, count($fields));
+		curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 	//execute post
-	$curl_result = curl_exec($ch);
+		$curl_result = curl_exec($ch);
 
 	//close connection
-	curl_close($ch);
-    oboe_log_exit("$($LayerPrefix)traceview-annotate");
+		curl_close($ch);
+    	oboe_log_exit("$($LayerPrefix)traceview-annotate");
     }
 
 }
